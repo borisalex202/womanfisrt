3ada161e30a24d3aedaa55afdf415d9f36d2b7ce
Alexander Boris <index.wow202@gmail.com>
2017-02-03 10:20:57 +0300

    Initialize the project and setting for working

---
+++ gulp/tasks/js.js
@@ -0,0 +1,49 @@
+var gulp        = require('gulp'),
+    browsersync = require('browser-sync'),
+    fileinclude = require('gulp-file-include'),
+    uglify      = require('gulp-uglify'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    config      = require('../config');
+
+/*
+ * Build js
+ */
+
+gulp.task('js', [
+    'js-internal',
+    'js-external'
+]);
+
+gulp.task('js-internal', config.wrapPipe(function(success, error) {
+    browsersync.notify('Compiling js');
+
+    return gulp.src(config.js.srcInternal)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file',
+            indent: true
+        }).on('error', error))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.js.development.dest))
+}));
+
+gulp.task('js-external', config.wrapPipe(function(success, error) {
+    browsersync.notify('Compiling js');
+
+    // jquery делаем отдельно чтобы подключить его в head
+    gulp.src(config.js.srcJquery)
+        .pipe(gulp.dest(config.js.development.destJquery));
+
+    // html5shiv делаем отдельно чтобы подключить его в head
+    gulp.src(config.js.srcHtml5shiv)
+        .pipe(gulp.dest(config.js.development.destHtml5shiv));
+
+    return gulp.src(config.js.srcExternal)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file',
+            indent: true
+        }).on('error', error))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.js.development.dest))
+}));
---
+++ src/js/external.js
@@ -0,0 +1,5 @@
+@@include('./partials/_bootstrap.js');
+@@include('./../../bower_components/slick-carousel/slick/slick.min.js');
+@@include('./../../bower_components/jquery-form-styler/jquery.formstyler.min.js');
+@@include('./../../bower_components/jscrollpane/script/jquery.mousewheel.js');
+@@include('./../../bower_components/jscrollpane/script/jquery.jscrollpane.min.js');
